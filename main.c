09945e57b3858526e8f0969846acb83b777a2b11
salomohutapea <hutapeasalomo@gmail.com>
2019-02-26 21:07:18 +0700

    Change the variables type to double, change the variables name, and added f(x)

--- main.c
+++ main.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
-float f(float x)
+double f(double x)
 {
     return(pow(1.5, x) - 5);
 }
 
-float df (float x)
+double df (double x)
 {
     return(pow(1.5, x) * (log(1.5)));
 }
-float main() {
+main() {
 
     int itr = 1;
-    float h, x0 = 10, x1, e = 0.01, rae;
+    double a = 10, b, e = 0.01, rae;
 
     do {
-        h = f(x0) / df(x0);
-        x1 = x0 - h;
-        rae = fabs((x1 - x0) / x1);
-        printf("\nIteration no-%d    x = %f\n", itr, x1);
+
+        b = a - f(a) / df(a);
+        rae = fabs((b - a) / b);
+        printf("\nIteration no-%d    x = %f    f(x) = %f\n", itr, b, f(b));
         printf("RAE in iteration no-%d is %f\n", itr, rae);
-        x0 = x1;
+        a = b;
         itr++;
     } while (rae > e);
 
-    printf("\nAfter %d iterations, root = %f\n", itr-1, x0);
+    printf("\nAfter %d iterations, root = %f\n", itr-1, b);
 
     return 0;
 }
